eabc472afe672fce599749f0ccc8e81b3b2a923c
dbenesch <dbenesch@critias.ca>
2022-02-07 23:31:50 -0500

    modified find_successive_intervals.py to accept only intervals without timestamps

--- neurokit2/misc/find_successive_intervals.py
+++ neurokit2/misc/find_successive_intervals.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 
-def find_successive_intervals(intervals, intervals_time, thresh_diff_ms=2, n_diff=1):
+def find_successive_intervals(intervals, intervals_time=None, thresh_unequal=2, n_diff=1):
     """Identify successive intervals
 
-    Identification of intervals that are consecutive 
+    Identification of intervals that are consecutive
     (e.g. in case of missing data).
 
     Parameters
@@ -13,11 +13,11 @@ def find_successive_intervals(intervals, intervals_time, thresh_diff_ms=2, n_dif
         Intervals, e.g. breath-to-breath (BBI) or rpeak-to-rpeak (RRI)
     intervals_time : list or ndarray
         Time points corresponding to intervals, in seconds.
-    thresh_diff_ms : int, float
-        Threshold at which the difference between time points is considered to 
+    thresh_unequal : int, float
+        Threshold at which the difference between time points is considered to
         be unequal to the interval, in milliseconds.
     n_diff: int
-        The number of times values are differenced. 
+        The number of times values are differenced.
         Can be used to check which values are valid for the n-th difference
         assuming successive intervals.
 
@@ -34,21 +34,26 @@ def find_successive_intervals(intervals, intervals_time, thresh_diff_ms=2, n_dif
     >>> successive_intervals = nk.find_successive_intervals(rri, rri_time)
     >>> successive_intervals
     array([ True, False,  True,  True])
+    >>> rri = [400, 500, np.nan, 700, 800, 900]
+    >>> successive_intervals = find_successive_intervals(rri)
+    >>> successive_intervals
+    array([ True, False, False,  True,  True])
     """
-    
+
     # Convert to numpy array
-    intervals_time = np.array(intervals_time)
     intervals = np.array(intervals)
 
-    # Remove the timestamps of the NaN intervals (if any)
-    intervals_time = intervals_time[~np.isnan(intervals)]
-    # Remove the NaN intervals (if any)
-    intervals = intervals[~np.isnan(intervals)]
-      
-    diff_intervals_time_ms = np.diff(intervals_time, n=n_diff)*1000
-    
-    abs_error_intervals_ref_time = abs(diff_intervals_time_ms - np.diff(intervals[1:], n=n_diff-1))
-    
-    successive_intervals = abs_error_intervals_ref_time <= thresh_diff_ms
-      
+    if intervals_time is None:
+        intervals_time = np.nancumsum(intervals / 1000)
+    else:
+        intervals_time = np.array(intervals_time)
+
+    intervals_time[np.isnan(intervals)] = np.nan
+
+    diff_intervals_time_ms = np.diff(intervals_time, n=n_diff) * 1000
+
+    abs_error_intervals_ref_time = abs(diff_intervals_time_ms - np.diff(intervals[1:], n=n_diff - 1))
+
+    successive_intervals = abs_error_intervals_ref_time <= thresh_unequal
+
     return np.array(successive_intervals)
